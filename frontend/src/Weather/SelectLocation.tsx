938038c7dcc7dd15585ab142de24abee66a8d126
Erik Ecoologic <erikecoologic@gmail.com>
2021-08-22 21:19:27 +1000

    SelectLocation mocked

--- frontend/src/Weather/SelectLocation.tsx
+++ frontend/src/Weather/SelectLocation.tsx
@@ -1,12 +1,38 @@
-import { FC } from 'react';
+import { Select } from '@material-ui/core';
+import { FC, useState } from 'react';
 import { Ilocation } from './WeatherApi';
 
 interface ISelectLocationProps {
   locations: Ilocation[];
 }
 
+interface ISelectOnChangeProps {
+  name?: string | undefined;
+  value: unknown;
+}
+
 const SelectLocation: FC<ISelectLocationProps> = ({ locations }) => {
-  return <p>Select from {JSON.stringify(locations)}</p>;
+  const [selection, setSelection] = useState<string | undefined>(undefined);
+  const onChange = (event: React.ChangeEvent<ISelectOnChangeProps>) => {
+    setSelection(event.target.name);
+    console.log(`SelectLocaiton ${event.target.name}`);
+  };
+  if (locations.length) {
+    return (
+      <Select
+        native
+        value={selection}
+        onChange={onChange}
+        inputProps={{ name: 'Location', id: 'locationSelect' }}
+      >
+        {locations.map((location) => (
+          <option value={location.woeid}>{location.name}</option>
+        ))}
+      </Select>
+    );
+  } else {
+    return null;
+  }
 };
 
 export default SelectLocation;
